4c7c2c6be5a05d9ed6f937c303b2b3f612159140
Ray Crist <rayhcrist@gmail.com>
2021-10-29 12:14:21 -0400

    Saving progess

--- CS98/Assets/Scripts/PlayerScripts/PlayerMove.cs
+++ CS98/Assets/Scripts/PlayerScripts/PlayerMove.cs
@@ -4,36 +4,44 @@ using UnityEngine;
 
 public class PlayerMove : MonoBehaviour
 {
-  public int movementSpeed;
   public int BALL_RANGE = 25;
+  private Vector2 BALL_CENTER;
+  private GameObject ball;
 
   private Vector2 pointA;
   private Vector2 pointB;
 
-  private GameObject ball;
-  private Vector2 BALL_CENTER;
-
-  private double angleSplit = 45;
+  private Touch touch;
 
   public float thrust = 10f;
 
-  // Use this for initialization
+  Camera camera;
+
+  // Save a reference to the joystick (should only be 1 per screen)
+  // and its center pivot point.
   void Start () {
     if (ball == null) {
         ball = GameObject.FindGameObjectsWithTag("JoystickBall")[0];
     }
-    BALL_CENTER = new Vector2(ball.GetComponent<RectTransform>().position.x, ball.GetComponent<RectTransform>().position.y);
+    camera = Camera.main;
+    BALL_CENTER = ball.GetComponent<RectTransform>().position;
+    Debug.Log(BALL_CENTER);
   }
 
   // Update is called once per frame
   void FixedUpdate () {
-    if (Input.GetMouseButton(0)) {
-      Vector2 touchPosition = new Vector2(Input.mousePosition.x, Input.mousePosition.y);
+    if (Input.touchCount > 0) {
+      touch = Input.GetTouch(0);
+      Vector2 touchPosition = new Vector2(touch.position.x, touch.position.y);
       Vector2 relativePosition = touchPosition - BALL_CENTER;
 
+      // Debug.Log(relativePosition);
+
+      // Get angle of joystick. 
       float relAngle = Vector2.Angle(Vector2.right, relativePosition);
       Vector2 playerDirection;
 
+      // Get the nearest cardinal direction of the joystick
       if (relAngle > 135) {
         playerDirection = Vector2.left;
       } else if (relAngle < 45) {
