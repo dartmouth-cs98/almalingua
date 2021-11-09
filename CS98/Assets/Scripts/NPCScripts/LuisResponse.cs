76115660645e2a49b0530e7ab6524b7177f36fb2
Ray Crist <rayhcrist@gmail.com>
2021-11-08 22:21:39 -0500

    delete unused files

--- CS98/Assets/Scripts/NPCScripts/LuisResponse.cs
+++
@@ -1,2 +0,0 @@
-// {"query":"k","prediction":{"topIntent":"Hello","intents":{"Hello":{"score":0.9004985},"Learn More":{"score":0.041805428},"None":{"score":0.0072326683}},"entities":{},"sentiment":{"label":"neutral","score":0.5}}}
-
