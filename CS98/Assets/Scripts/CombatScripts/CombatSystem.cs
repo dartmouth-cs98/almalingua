9c1635b90ffa3591a27f9898fbe1fbe71e9285b0
Vi2x101 <65918376+Vi2x101@users.noreply.github.com>
2022-01-17 14:58:26 -0500

    finished basic combat system

--- CS98/Assets/Scripts/CombatScripts/CombatSystem.cs
+++ CS98/Assets/Scripts/CombatScripts/CombatSystem.cs
@@ -44,6 +44,7 @@ public class CombatSystem : MonoBehaviour
         yield return new WaitForSeconds(2f);
 
         state = CombatState.PLAYERTURN;
+        PlayerTurn();
     }
 
     void PlayerTurn()
@@ -51,5 +52,98 @@ public class CombatSystem : MonoBehaviour
         dialogueText.GetComponent<TMPro.TextMeshProUGUI>().text = "Choose an action: ";
     }
 
+
+    IEnumerator PlayerAttack()
+    {
+        // Damage the enemy
+        bool isDead = enemyUnit.TakeDamage(playerUnit.damage);
+
+        enemyHUD.SetHP(enemyUnit.currentHP);
+        dialogueText.GetComponent<TMPro.TextMeshProUGUI>().text = "The attack is successful!";
+
+        yield return new WaitForSeconds(2f);
+
+        // Check if the enemy is dead
+        if (isDead)
+        {
+            // End the battle
+            state = CombatState.WON;
+            EndBattle();
+        }
+        else
+        {
+            // Enemy's turn
+            state = CombatState.ENEMYTURN;
+            StartCoroutine(EnemyTurn());
+        }
+    }
+
+    IEnumerator PlayerHeal()
+    {
+        playerUnit.Heal(10);
+
+        playerHUD.SetHP(playerUnit.currentHP);
+        dialogueText.GetComponent<TMPro.TextMeshProUGUI>().text = "You feel renewed strength!";
+
+        yield return new WaitForSeconds(2f);
+
+        state = CombatState.ENEMYTURN;
+        StartCoroutine(EnemyTurn());
+    }
+    
+
+    IEnumerator EnemyTurn()
+    {
+        dialogueText.GetComponent<TMPro.TextMeshProUGUI>().text = enemyUnit.unitName + " attacks!";
+
+        yield return new WaitForSeconds(1f);
+
+        bool isDead = playerUnit.TakeDamage(enemyUnit.damage);
+
+        playerHUD.SetHP(playerUnit.currentHP);
+
+        yield return new WaitForSeconds(1f);
+
+        if (isDead)
+        {
+            state = CombatState.LOST;
+            EndBattle();
+        }
+        else 
+        {
+            state = CombatState.PLAYERTURN;
+            PlayerTurn();
+        }
+
+    }
+
+    void EndBattle()
+    {
+        if (state == CombatState.WON)
+        {
+            dialogueText.GetComponent<TMPro.TextMeshProUGUI>().text = "You won the battle!";
+        }
+        else if (state == CombatState.LOST)
+        {
+            // Return to the Witch's House
+            dialogueText.GetComponent<TMPro.TextMeshProUGUI>().text = "You were defeated!";
+        }
+    }
+
+    public void OnAttackButton()
+    {
+        if (state != CombatState.PLAYERTURN)
+            return;
+        
+        StartCoroutine(PlayerAttack());
+    }
+
+    public void OnHealButton()
+    {
+        if (state != CombatState.PLAYERTURN)
+            return;
+        
+        StartCoroutine(PlayerHeal());
+    }
 
 }
