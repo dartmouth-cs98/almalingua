9fa212739a6f903967f41d6beb27332a620a3306
Vi2x101 <65918376+Vi2x101@users.noreply.github.com>
2022-01-17 14:14:34 -0500

    set up all UI and Battle Hubs

--- CS98/Assets/Scripts/CombatScripts/CombatSystem.cs
+++ CS98/Assets/Scripts/CombatScripts/CombatSystem.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public enum CombatState { START, PLAYERTURN, ENEMYTURN, WON, LOST}
 public class CombatSystem : MonoBehaviour
@@ -10,19 +11,44 @@ public class CombatSystem : MonoBehaviour
     public Transform  playerCombatStation;
     public Transform enemyCombatStation;
 
+    Unit playerUnit;
+    Unit enemyUnit;
+
+    public GameObject dialogueText;
+
+    public BattleHUD playerHUD;
+    public BattleHUD enemyHUD;
+
     public CombatState state;
 
     // Start is called before the first frame update
     void Start()
     {
         state = CombatState.START;
-        SetupCombat();
+        StartCoroutine(SetupCombat());
     }
 
-    void SetupCombat()
+    IEnumerator SetupCombat()
     {
-        Instantiate(playerPrefab, playerCombatStation);
-        Instantiate(enemyPrefab, enemyCombatStation);
+        GameObject playerGO = Instantiate(playerPrefab, playerCombatStation);
+        playerUnit = playerGO.GetComponent<Unit>();
+
+        GameObject enemyGO = Instantiate(enemyPrefab, enemyCombatStation);
+        enemyUnit = enemyGO.GetComponent<Unit>();
+
+        dialogueText.GetComponent<TMPro.TextMeshProUGUI>().text = "A wild " + enemyUnit.unitName + " approaches...";
+
+        playerHUD.SetHUD(playerUnit);
+        enemyHUD.SetHUD(enemyUnit);
+
+        yield return new WaitForSeconds(2f);
+
+        state = CombatState.PLAYERTURN;
+    }
+
+    void PlayerTurn()
+    {
+        dialogueText.GetComponent<TMPro.TextMeshProUGUI>().text = "Choose an action: ";
     }
 
 
