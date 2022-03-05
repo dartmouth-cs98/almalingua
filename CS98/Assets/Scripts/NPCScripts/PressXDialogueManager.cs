bcf03b8891852e5738383fd61e036d6c154c6534
Ray Crist <rayhcrist@gmail.com>
2022-03-04 20:07:16 -0500

    Enable X to start dialogue

--- CS98/Assets/Scripts/NPCScripts/PressXDialogueManager.cs
+++ CS98/Assets/Scripts/NPCScripts/PressXDialogueManager.cs
@@ -2,19 +2,30 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class DialogueUIExplainer : MonoBehaviour
+public class PressXDialogueManager : MonoBehaviour
 {
 
+    bool inTriggerSpace = false;
     public GameObject InfoUI;
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.X) && inTriggerSpace)
+        {
+            gameObject.GetComponent<NPCInteraction>().StartDialogue();
+        }
+    }
 
     void OnTriggerEnter2D(Collider2D col)
     {
         InfoUI.GetComponent<HideShowObjects>().Show();
+        inTriggerSpace = true;
     }
 
     void OnTriggerExit2D(Collider2D col)
     {
         InfoUI.GetComponent<HideShowObjects>().Hide();
+        inTriggerSpace = false;
     }
 
 }
