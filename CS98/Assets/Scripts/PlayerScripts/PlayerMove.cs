ae9cd51040048f1bcd933b38e7a9ebf9ebbfd2f1
Sada R. Nichols-Worley <sada.r.nichols-worley.22@dartmouth.edu>
2022-03-07 15:59:45 -0500

    fixing teleportation to village rather than to witch's house directly

--- CS98/Assets/Scripts/PlayerScripts/PlayerMove.cs
+++ CS98/Assets/Scripts/PlayerScripts/PlayerMove.cs
@@ -17,7 +17,7 @@ public class PlayerMove : MonoBehaviour
     Vector2[] playerPositions = new Vector2[] {new Vector2((float)0.5, (float)-1), 
     new Vector2((float)-9.3, (float)-20), new Vector2((float)48, (float)-1.4), 
     new Vector2((float)-3.5, (float)16.2), new Vector2((float)-57, (float)-2.97), new Vector2((float) -9.5, (float)17.8),
-    new Vector2((float)-5.04, (float)-19), new Vector2((float)-54, (float)-10)};
+    new Vector2((float)-5.04, (float)-19), new Vector2((float)0, (float)0)};
 
     // Use this for initialization
     void Start()
